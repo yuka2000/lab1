eb6939fba241292878be3f3be0cf9105be3e7bc0
yuka2000 <144158157+yuka2000@users.noreply.github.com>
2024-06-20 10:30:24 -0400

    Update index2.js

--- index2.js
+++ index2.js
@@ -1,11 +1,15 @@
+//fs module using require(), which allows us to access filesystem-related functions like readFile.
+This approach is typical in CommonJS modules used in Node.js applications.
+
 const fs = require("node:fs");
 
+//readFile function from the fs module to read the contents of a file named text.txt.
 
 fs.readFile("./text.txt", "utf-8", (err, data)=>{
     if (err)
-    
+    //t is thrown to terminate execution and display the error message.
     throw err;
-
+//the content of the file (data) is logged to the console.
     console.log(data);
 
 })
